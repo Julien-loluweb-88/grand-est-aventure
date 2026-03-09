1c9766da2245bce535a7aae4a8000a37333f76cb
Haruka-Izumi18 <haruka.izumi18@gmail.com>
2026-03-09 16:37:59 +0100

    formulaire admin game

--- src/app/(authenticated)/admin-game/page.tsx
+++ src/app/(authenticated)/admin-game/page.tsx
@@ -1,10 +1,49 @@
+import { Button } from "@/components/ui/button"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
     <div>
       <h1>Admin Game</h1>
-      <div>ici formulaire de connexion</div>
+      <div>
+     <FieldGroup>
+      <FieldDescription>
+          Bonjour!
+        </FieldDescription>
+      <Field>
+        <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
+        <Input className="input-admin" id="fieldgroup-name" placeholder="Jordan Lee" />
+      </Field>
+      <Field>
+        <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
+        <Input
+        className="input-admin"
+          id="fieldgroup-email"
+          type="email"
+          placeholder="name@example.com"
+        />
+         <Field>
+        <FieldLabel htmlFor="fieldgroup-name">Mots de pass</FieldLabel>
+        <Input className="input-admin" id="fieldgroup-password" placeholder="votre mots de pass" />
+      </Field>
+      
+      </Field>
+      <Field orientation="horizontal">
+        <Button type="reset" variant="outline">
+          Reset
+        </Button>
+        <Button type="submit">Submit</Button>
+      </Field>
+    </FieldGroup>
+</div>
     </div>
-
   );
 }
+
+
