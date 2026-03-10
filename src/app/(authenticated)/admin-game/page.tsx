4181ae4a2f9458adc4f600b48265350fd14d8f7d
Haruka-Izumi18 <haruka.izumi18@gmail.com>
2026-03-10 16:35:02 +0100

    components login-form

--- src/app/(authenticated)/admin-game/page.tsx
+++ src/app/(authenticated)/admin-game/page.tsx
@@ -1,21 +1,14 @@
 "use client"
-import { authClient, signIn } from "@/lib/auth-client"
-import { Button } from "@/components/ui/button"
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+import { authClient } from "@/lib/auth-client"
+
 import { useState } from "react"
 import { useRouter } from "next/navigation"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-
+import { LoginFormComponent, SignUpFormComponent  } from "@/components/login-form"
 export default function Home() {
   const router = useRouter()
 // State Inscription 
-  const [dataForm, setDataForm] = useState({
+  const [signUpForm, setSignUpForm] = useState({
     name: "",
     email: "",
     password: "",
@@ -29,33 +22,6 @@ export default function Home() {
   })
 
   const [loading, setLoading] = useState(false)
-
-  // Inscription 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    if(dataForm.password !== dataForm.confirmPassword){
-      alert("Les mots de passe ne correspondent pas")
-      return
-    }
-  setLoading(true)
-  console.log("je suis la data", dataForm)
-  const { data, error } = await authClient.signUp.email({
-  name: dataForm.name,
-  email: dataForm.email,
-  password: dataForm.password,
-  });
-
-  setLoading(false)
-
-  if (error) {
-    console.log(error)
-    alert(error.message)
-    return
-  }
-
-  console.log(data)
-  router.push("/dashboard")
-  }
 
   // Connexion
   const handleSignIn = async (e: React.FormEvent) => {
@@ -77,117 +43,59 @@ export default function Home() {
     router.push("/dashboard")
   }
 
-  return (
-    
-      <><h1>Admin Game</h1>
-      <div className="flex gap-10 justify-center items-start">
+  // Inscription 
+  const handleSignUp = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if(signUpForm.password !== signUpForm.confirmPassword){
+      alert("Les mots de passe ne correspondent pas")
+      return
+    }
+  setLoading(true)
+  console.log("je suis la data", signUpForm)
+  const { data, error } = await authClient.signUp.email({
+  name: signUpForm.name,
+  email: signUpForm.email,
+  password: signUpForm.password
+  });
 
-      <Tabs defaultValue="signin" className="w-[400px]">
+  setLoading(false)
+
+  if (error) {
+    console.log(error)
+    alert(error.message)
+    return
+  }
+
+  console.log(data)
+  router.push("/dashboard")
+  }
+
+  return (
+      <><h1>Admin Game</h1>
+      <div className="w-screen p-10 flex justify-center items-center">
+        <Tabs defaultValue="signin" className="w-[400px]">
   <TabsList>
     <TabsTrigger value="signin">Connexion</TabsTrigger>
     <TabsTrigger value="signup">Inscription</TabsTrigger>
   </TabsList>
   <TabsContent value="signin"> 
-      {/*Connexion */}
-      <div className="gap-10 w-[400px]">
-        <form onSubmit={handleSignIn}>
-          <FieldGroup>
-            <FieldDescription>
-              Connextion d&apos;admin
-            </FieldDescription>
-            <Field>
-              <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
-              <Input
-                className="input-admin"
-                id="fieldgroup-email"
-                type="email"
-                placeholder="name@example.com"
-                value={signInForm.email}
-                onChange={(e) => setSignInForm({ ...signInForm, email: e.target.value })} />
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="fieldgroup-name">Mots de pass</FieldLabel>
-              <Input
-                className="input-admin"
-                id="fieldgroup-password"
-                placeholder="votre mot de passe"
-                type="password"
-                value={signInForm.password} onChange={(e) => setSignInForm({ ...signInForm, password: e.target.value })} />
-            </Field>
-
-            <Field orientation="horizontal">
-              <Button type="submit" disabled={loading}>
-                {loading ? "Connexion..." : "Se connecter"}
-              </Button>
-            </Field>
-          </FieldGroup>
-        </form>
-      </div>
-      </TabsContent>
-  <TabsContent value="signup">
-  <div className="w-[400px]">
-        <form onSubmit={handleSubmit}>
-          <FieldGroup>
-            <FieldDescription>
-              Inscription d&apos;admin
-            </FieldDescription>
-            <Field>
-              <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
-              <Input className="input-admin"
-                id="fieldgroup-name"
-                placeholder="Jordan Lee"
-                value={dataForm.name} onChange={(e) => setDataForm({ ...dataForm, name: e.target.value })} />
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
-              <Input
-                className="input-admin"
-                id="fieldgroup-email"
-                type="email"
-                placeholder="name@example.com"
-                value={dataForm.email}
-                onChange={(e) => setDataForm({ ...dataForm, email: e.target.value })} />
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="fieldgroup-name">Mots de pass</FieldLabel>
-              <Input
-                className="input-admin"
-                id="fieldgroup-password"
-                placeholder="votre mot de passe"
-                type="password"
-                value={dataForm.password} onChange={(e) => setDataForm({ ...dataForm, password: e.target.value })} />
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="fieldgroup-name">Confirmez le mots de pass</FieldLabel>
-              <Input
-                className="input-admin"
-                id="fieldgroup-password"
-                placeholder="Confirmez votre mot de passe"
-                type="password"
-                value={dataForm.confirmPassword} onChange={(e) => setDataForm({ ...dataForm, confirmPassword: e.target.value })} />
-            </Field>
-
-            <Field orientation="horizontal">
-              <Button type="reset" variant="outline">
-                Reset
-              </Button>
-
-              <Button type="submit">Submit</Button>
-            </Field>
-          </FieldGroup>
-        </form>
-      </div>
-  </TabsContent>
- 
-</Tabs>
-
-     
-     
-      
-  
-
-   
-
-    </div></>
+      <LoginFormComponent
+  signInForm={signInForm}
+  setSignInForm={setSignInForm}
+  handleSignIn={handleSignIn}
+  loading={loading}
+/> 
+</TabsContent>
+<TabsContent value="signup">
+ <SignUpFormComponent
+  signUpForm={signUpForm}
+  setSignUpForm={setSignUpForm}
+  handleSignUp={handleSignUp}
+  loading={loading}
+/>   
+</TabsContent>
+</Tabs> 
+    </div>
+    </>
   );
 }
---
+++ src/components/login-form.tsx
@@ -0,0 +1,224 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+type LoginFormProps = {
+  className?: string
+  signInForm: {
+    email: string
+    password: string
+  }
+  setSignInForm: React.Dispatch<
+    React.SetStateAction<{
+      email: string
+      password: string
+    }>
+  >
+  handleSignIn: (e: React.FormEvent<HTMLFormElement>) => void
+  loading: boolean
+}
+
+export function LoginFormComponent({
+  className,
+  signInForm,
+  setSignInForm,
+  handleSignIn,
+  loading,
+}: LoginFormProps) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Formulaire de connexion</CardTitle>
+          <CardDescription>
+            Entrez votre adresse e-mail ci-dessous pour vous connecter à votre compte.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSignIn}>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  value={signInForm.email}
+                  onChange={(e) =>
+                    setSignInForm({ ...signInForm, email: e.target.value })
+                  }
+                  required
+                />
+              </Field>
+
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Mot de passe</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Mot de passe oublié ?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  value={signInForm.password}
+                  onChange={(e) =>
+                    setSignInForm({ ...signInForm, password: e.target.value })
+                  }
+                  required
+                />
+              </Field>
+
+              <Field className="flex flex-col gap-3">
+                <Button type="submit" disabled={loading}>
+                  {loading ? "Connexion..." : "Login"}
+                </Button>
+
+                <Button variant="outline" type="button">
+                  Se connecter avec Google
+                </Button>
+
+                <FieldDescription className="text-center">
+                  Vous n&apos;avez pas encore de compte ? <a href="#">Inscrivez-vous</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+type SignUpFormProps = {
+  className?: string
+  signUpForm: {
+    name: string
+    email: string
+    password: string
+    confirmPassword: string
+  }
+  setSignUpForm: React.Dispatch<
+    React.SetStateAction<{
+      name: string
+      email: string
+      password: string
+      confirmPassword: string
+    }>
+  >
+  handleSignUp: (e: React.FormEvent<HTMLFormElement>) => void
+  loading: boolean
+}
+
+export function SignUpFormComponent({
+  signUpForm,
+  setSignUpForm,
+  handleSignUp,
+  loading,
+}: SignUpFormProps) {
+  return (
+    <div className={cn("flex flex-col gap-6")}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Formulaire de inscription</CardTitle>
+          <CardDescription>
+            Entrez votre nom, votre adresse e-mail et votre mots de pass ci-dessous pour créer votre compte.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSignUp}>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="fieldgroup-name">Votre nom</FieldLabel>
+                <Input
+                  id="fieldgroup-name"
+                  placeholder="Jordan Lee"
+                    value={signUpForm.name}
+                  onChange={(e) =>
+                    setSignUpForm({ ...signUpForm, name: e.target.value })
+                  }
+                  required
+                />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  value={signUpForm.email}
+                  onChange={(e) =>
+                    setSignUpForm({ ...signUpForm, email: e.target.value })
+                  }
+                  required
+                />
+              </Field>
+
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Mot de passe</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Mot de passe oublié ?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  placeholder="Votre mot de passe"
+                  type="password"
+                  value={signUpForm.password}
+                  onChange={(e) =>
+                    setSignUpForm({ ...signUpForm, password: e.target.value })
+                  }
+                  required
+                />
+              </Field>
+              <Field>
+              <FieldLabel htmlFor="fieldgroup-name">Confirmez le mots de pass</FieldLabel>
+              <Input
+                id="confirmPassword"
+                placeholder="Confirmez votre mot de passe"
+                type="password"
+                value={signUpForm.confirmPassword} onChange={(e) => setSignUpForm({ ...signUpForm, confirmPassword: e.target.value })} />
+            </Field>
+
+              <Field className="flex flex-col gap-3">
+                <Button type="submit" disabled={loading}>
+                  {loading ? "Connexion..." : "Login"}
+                </Button>
+
+                <Button variant="outline" type="button">
+                  S&apos;inscrir avec Google
+                </Button>
+
+                <FieldDescription className="text-center">
+                  Vous n&apos;avez pas encore de compte ? <a href="#">Inscrivez-vous</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
